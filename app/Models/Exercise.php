9dcef029e64299b6462299172ef7750c0e1c179a
Sapna615 <142426825+Sapna615@users.noreply.github.com>
2026-05-19 15:43:12 +0530

    fix: add missing exercises() and workouts() relationships to models - was causing deploy crash

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -7,4 +7,9 @@ use MongoDB\Laravel\Eloquent\Model;
 class Exercise extends Model
 {
     protected $fillable = ['name', 'muscle_group', 'video_url', 'instructions'];
+
+    public function workouts()
+    {
+        return $this->belongsToMany(Workout::class);
+    }
 }
--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -7,4 +7,9 @@ use MongoDB\Laravel\Eloquent\Model;
 class Workout extends Model
 {
     protected $fillable = ['title', 'description', 'difficulty_level'];
+
+    public function exercises()
+    {
+        return $this->belongsToMany(Exercise::class);
+    }
 }
